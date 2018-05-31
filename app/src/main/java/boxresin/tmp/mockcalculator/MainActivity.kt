de12118338e1f0e487e8802d81a938ad8aee64bf
Alan <BoxResin@gmail.com>
2018-05-31 12:26:22 +0900

    mock-math 잘못된 패키지 경로 수정 및 기본 예제 구현

--- app/src/main/java/boxresin/tmp/mockcalculator/MainActivity.kt
+++ app/src/main/java/boxresin/tmp/mockcalculator/MainActivity.kt
@@ -2,11 +2,16 @@ package boxresin.tmp.mockcalculator
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import boxresin.tmp.lib.mockmath.maxN
+import boxresin.tmp.lib.mockmath.minN
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		text.text = "${maxN(7, 3, 6, 10, 4, 1)}\n${minN(7, 3, 6, 10, 4, 1)}"
 	}
 }
