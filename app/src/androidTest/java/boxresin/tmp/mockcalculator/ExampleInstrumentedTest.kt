48700cc10031cbdc76013647d4fb9479541ab1f5
Minsuk Eom <boxresin@gmail.com>
2018-05-31 11:39:51 +0900

    첫 번째 커밋

---
+++ app/src/androidTest/java/boxresin/tmp/mockcalculator/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package boxresin.tmp.mockcalculator
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+	@Test
+	fun useAppContext() {
+		// Context of the app under test.
+		val appContext = InstrumentationRegistry.getTargetContext()
+		assertEquals("boxresin.tmp.mockcalculator", appContext.packageName)
+	}
+}
